5e77b5ffae8c40706593ee06a206a91316940f4e
Juan Negron <negronjl@xtremeghost.com>
2017-03-06 06:28:56 -0500

    First pass at file objects

--- files.go
+++ files.go
@@ -70,13 +70,50 @@ func resourceS3FileCreate(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceS3FileRead(d *schema.ResourceData, meta interface{}) error {
+	debug := d.Get("debug").(bool)
+	bucket := d.Get("bucket").(string)
+	name := d.Get("name").(string)
+	file_path := d.Get("file_path").(string)
+	s3_client := meta.(*s3Client).s3Client
+
+	if debug {
+		log.Printf("[DEBUG] Reading file [%s] from bucket [%s] into file [%s]", name, bucket, file_path)
+	}
+
+	err := s3_client.FGetObject(bucket, name, file_path)
+	if err != nil {
+		log.Printf("[FATAL]  Unable to read file [%s] from bucket [%s] into file [%s]", name, bucket, file_path)
+		return errors.New(fmt.Sprintf("Unable to read file [%s]", name))
+	}
+
+	if debug {
+		log.Printf("[DEBUG] Read file [%s] from bucket [%s]", name, bucket)
+	}
 	return nil
 }
 
 func resourceS3FileUpdate(d *schema.ResourceData, meta interface{}) error {
-	return nil
+	return resourceS3BucketCreate(d, meta)
 }
 
 func resourceS3FileDelete(d *schema.ResourceData, meta interface{}) error {
+	debug := d.Get("debug").(bool)
+	bucket := d.Get("bucket").(string)
+	name := d.Get("name").(string)
+	s3_client := meta.(*s3Client).s3Client
+
+	if debug {
+		log.Printf("[DEBUG] Deleting file [%s] from bucket [%s]", name, bucket)
+	}
+
+	err := s3_client.RemoveObject(bucket, name)
+	if err != nil {
+		log.Printf("[FATAL] Unable to delete file [%s] from bucket [%s]", name, bucket)
+		return errors.New(fmt.Sprintf("Unable to delete file [%s] from bucket [%s]", name, bucket))
+	}
+
+	if debug {
+		log.Printf("[DEBUG] Deleted file [%s] fro bucket [%s]", name, bucket)
+	}
 	return nil
 }
